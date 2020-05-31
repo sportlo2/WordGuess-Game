a9b67ef67112ecd3d511dff06d2a0806d53b93dd
sportlo2 <portlock.sierra@gmail.com>
2020-05-30 20:07:29 -0400

    psuedo coded js file

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+// what happens when you press any key
+// - game begins and blank underscore (1 per character) appears
+// - add letter string to letters guessed section
+// - conditional statement when letter key is press
+//      - if letter is found in string, then it is revealed under whats the word?
+//      - if not then number of guesses remaining decreases
+// game ends
+// - all blank underscores are revealed; 
+//      - Wins adds 1, and game starts over. repeat from step 1
+// - max number of guesses is reached, word is revealed, game starts over
